fb01eaaa50b9ec82cb9001501ececaa5439cb8dc
Yassine Hamza <yassine.hamza@esprit.tn>
2024-06-05 16:35:20 +0100

    back

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(express.json())
+app.use(cors({
+  origin: 'http://localhost:5173' // Allow requests from localhost:3000
+}));
+
+app.use("/", require("./routes/music"));
+
+
+// app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+// Handles any requests that don't match the ones above
+// app.get('*', (req, res) =>{
+//     res.sendFile(path.join(__dirname+'../frontend/dist/index.html'));
+// });
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, console.log(`Server started on port ${PORT}`));
+  
+module.exports = app;
---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Stop the script if any command fails
+set -e
+
+# Install Node packages
+echo "Installing Node.js dependencies..."
+npm install
+
+# Install Python packages
+echo "Installing Python dependencies..."
+pip install -r requirements.txt
+
+# Any other build steps can be added here
