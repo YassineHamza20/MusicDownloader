42b6e0312f6ae82b4f79181ecb0ec8ba3ac528ab
Yassine Hamza <yassine.hamza@esprit.tn>
2024-06-07 19:21:21 +0100

    back100

--- routes/music.js
+++ routes/music.js
@@ -5,11 +5,9 @@ const router = express.Router();
 
  
 const isValidYouTubeUrl = (url) => {
-  const pattern = /^(https?:\/\/)?((www\.youtube\.com\/(watch\?v=|shorts\/))|(youtu\.be\/))[a-zA-Z0-9_-]{11}(\?.*)?$/;
-  return pattern.test(url);
+    const pattern = /^(https?:\/\/)?(www\.youtube\.com|youtu\.?be)\/(watch\?v=|shorts\/)?[a-zA-Z0-9_-]{11}$/;
+    return pattern.test(url);
 };
-
-
 
   const isValidPlaylistUrl = (url) => {
     const pattern = /^(https?:\/\/)?(www\.)?youtube\.com\/playlist\?list=[\w-]+(&[^\s]*)?$/;
