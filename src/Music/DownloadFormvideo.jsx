5f2f14d19406a4de7458796510773b2ec5cc40b5
Yassine Hamza <yassine.hamza@esprit.tn>
2024-06-07 01:18:31 +0100

    front19

--- src/Music/DownloadFormvideo.jsx
+++ src/Music/DownloadFormvideo.jsx
@@ -13,31 +13,50 @@ function VideoDownloader() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     setLoading(true);
-    
-    try {
-      const response = await fetch('https://musicdownloader1.onrender.com/video', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ youtube_url: youtubeUrl })
-      });
 
-      const data = await response.json();
-      if (response.ok) {
-        setMessage(data.message);
-        setIsSuccess(true);
-        toast.success('Song downloaded successfully');
-      } else {
-        setMessage(`Error: ${data.message}`);
-        setIsSuccess(false);
-      }
+    try {
+        const response = await fetch('https://musicdownloader1.onrender.com/video', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ youtube_url: youtubeUrl })
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            if (data.success) {
+                const downloadUrl = data.downloadUrl;
+                const link = document.createElement('a');
+                link.href = downloadUrl;
+                link.setAttribute('download', ''); // Let the browser use the filename from the URL
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+
+                setMessage('video downloaded successfully');
+                setIsSuccess(true);
+                toast.success('video downloaded successfully');
+            } else {
+                setMessage(`Error: ${data.message}`);
+                setIsSuccess(false);
+                toast.error(`Error: ${data.message}`);
+            }
+        } else {
+            const errorData = await response.json();
+            setMessage(`Error: ${errorData.message}`);
+            setIsSuccess(false);
+            toast.error(`Error: ${errorData.message}`);
+        }
     } catch (error) {
-      console.error('Error:', error);
-      setMessage('Internal server error');
-      setIsSuccess(false);
+        console.error('Error:', error);
+        setMessage('Internal server error');
+        setIsSuccess(false);
+        toast.error('Internal server error');
     } finally {
-      setLoading(false);
+        setLoading(false);
     }
-  };
+};
+
+  
 
   return (
     <>
