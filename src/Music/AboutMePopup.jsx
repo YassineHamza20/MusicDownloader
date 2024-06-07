1f72402f3ef047ed38eb6cc282ecac98b53a4850
Yassine Hamza <yassine.hamza@esprit.tn>
2024-06-07 05:07:20 +0100

    front37

--- src/Music/AboutMePopup.jsx
+++ src/Music/AboutMePopup.jsx
@@ -10,10 +10,11 @@ function AboutMePopup() {
       <p>
         Hello, my name is Yassine, and I'm a computer science student
         with a passion for programming, development, and music. Combining my interests, I've created this high-quality
-        music website for people like you who love to enjoy good,
-        free music while studying or working.
-        <br></br>Feel free to reach out via  
-        <a href="https://www.linkedin.com/in/yassine-hamza-222605215">LinkedIn</a> 
+        music website for people like you who love to enjoy good 
+        music while studying or working.
+         
+       
+      
       </p>  
     </div>
   );
