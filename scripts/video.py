7542a741c20bd73831be1cedaf0ac8b8856f9ee5
Yassine Hamza <yassine.hamza@esprit.tn>
2024-06-07 20:19:53 +0100

    back111

--- scripts/video.py
+++ scripts/video.py
@@ -19,27 +19,20 @@ def embed_album_art_ffmpeg(video_path, image_path):
     """Embeds album art into an MP4 file using FFmpeg."""
     output_path = video_path.with_suffix('.temp.mp4')
     cmd = [
-        ffmpeg_path, '-i', str(video_path), '-i', str(image_image_path),
-        '-map', '0:v', '-map', '0:a', '-map', '1:v',  # Explicit stream mapping
-        '-c:v', 'copy', '-c:a', 'copy', '-c:v:2', 'mjpeg',  # Explicit codec specification for image
-        '-disposition:v:2', 'attached_pic',
-        '-metadata:s:v:2', 'title="Album cover"', '-metadata:s:v:2', 'comment="Cover (front)"',
+        ffmpeg_path, '-i', str(video_path), '-i', str(image_path),
+        '-map', '0', '-map', '1', '-c', 'copy', '-disposition:v:1', 'attached_pic',
+        '-metadata:s:v:1', 'title="Album cover"', '-metadata:s:v:1', 'comment="Cover (front)"',
         str(output_path)
     ]
     try:
         result = subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if result.stderr:
             print("FFmpeg stderr:", result.stderr.decode())
-        if result.stdout:
-            print("FFmpeg stdout:", result.stdout.decode())
-    except subprocess.CalledProcess.rror as e:
+    except subprocess.CalledProcessError as e:
         print("FFmpeg command failed with error:", e.stderr.decode())
         raise e
 
     os.replace(output_path, video_path)
-
-
-
 
 def download_video_as_mp4(youtube_url, output_folder):
     try:
@@ -47,13 +40,9 @@ def download_video_as_mp4(youtube_url, output_folder):
         title = sanitize_filename(yt.title)
         folder_path = Path(output_folder)
         folder_path.mkdir(parents=True, exist_ok=True)
-
-        # Enhanced video stream selection to prioritize 60fps and highest resolution
-        video_streams = yt.streams.filter(file_extension='mp4', progressive=True)
-        video = video_streams.order_by('resolution').order_by('fps').desc().first()
-        
+        video = yt.streams.filter(file_extension='mp4').get_highest_resolution()
         output_path = folder_path / f"{title}.mp4"
-        video.download(output_path=folder_path)
+        video.download(output_path=folder_path, filename=f"{title}.mp4")
 
         # Download thumbnail
         thumb_url = yt.thumbnail_url
