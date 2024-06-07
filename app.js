738a45e0caa5ae0b318ab2d9c810fe025e6bda31
Yassine Hamza <yassine.hamza@esprit.tn>
2024-06-07 01:54:09 +0100

    back76

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors(corsOptions));
 // Serve static files from the 'public' directory
 app.use('/downloads', express.static(path.join(__dirname, 'public'), {
   setHeaders: (res, path) => {
-      res.setHeader('Content-Disposition', 'attachment; filename="' + path.split('/').pop() + '"');
+      res.setHeader('Content-Disposition', `attachment; filename="${path.split('/').pop()}"`);
   }
 }));
 // Apply the router
