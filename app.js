18999d417333db29529b31300cf070c8691a53b3
Yassine Hamza <yassine.hamza@esprit.tn>
2024-06-08 22:23:01 +0100

    back118

--- app.js
+++ app.js
@@ -39,6 +39,18 @@ app.get('/', (req, res) => {
     res.send('Backend is running');
 });
 
- 
+app.get('/heartbeat', (req, res) => {
+  res.status(200).send('Server is awake!');
+});
+function keepServerAwake() {
+  const url = "https://musicdownloader1.onrender.com"; // Change to your actual server URL
+  fetch(url).then(response => response.text()).then(console.log).catch(console.error);
+
+  // Set timeout for next ping
+  setTimeout(keepServerAwake, 14 * 60 * 1000 + 50 * 1000); // 14 minutes and 50 seconds
+}
+
+// Start the pinging process
+keepServerAwake();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
