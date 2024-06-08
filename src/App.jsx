3fadb57f34ba0c11833efd8fe83bdd0565f26f8e
Yassine Hamza <yassine.hamza@esprit.tn>
2024-06-08 20:14:32 +0100

    front43

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MusicDownloader from './Music/DownloadForm.jsx';
 import PlaylistDownloader from './Music/Playlist.jsx';
 import PlaylistDownloaderVideo from './Music/Playlistvideo.jsx';
 import VideoDownloader from './Music/DownloadFormvideo.jsx';
-import './App.css';
-import { ToastContainer } from 'react-toastify';
 import NotFound from './Music/NotFound.jsx';
 import ShareButton from './Music/Share.jsx';
 import AboutMePopup from './Music/AboutMePopup.jsx';
- 
+import { ToastContainer } from 'react-toastify';
+import './App.css';
 
 function App() {
   const [showPopup, setShowPopup] = useState(false);
@@ -20,16 +19,15 @@ function App() {
 
   return (
     <>
-    
-      <div style={{ marginBottom: '0px', textAlign: 'center' }}>
-        <img src="/Melody.png" alt="MelodyAddictLogo" style={{ marginRight: '930px',width: '100px', animation: 'logo-spin infinite 10s linear' }} />
-        <h3 style={{  marginTop: '-100px',color: 'white' }}>Fastest High Quality Music Downloader (320kbps)</h3>
+      <div style={{ textAlign: 'center' }}>
+        <img src="/Melody.png" alt="MelodyAddict Logo" style={{ marginRight: '930px', width: '100px', animation: 'logo-spin infinite 10s linear' }} />
+        <h3 style={{ marginTop: '-100px', color: 'white' }}>Fastest High-Quality Music Downloader (320kbps)</h3>
       </div>
-    <div style={{ marginBottom: '60px', textAlign: 'center' }}>
-    <button onClick={toggleComponentView} style={{
+      <div style={{ marginBottom: '60px', textAlign: 'center' }}>
+        <button onClick={toggleComponentView} style={{
           backgroundColor: '#081d48', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '20px', cursor: 'pointer', fontSize: '16px'
-        }}> MP4/MP3</button>
-    </div>
+        }}>Toggle MP4/MP3</button>
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={
@@ -50,13 +48,12 @@ function App() {
         </Routes>
       </BrowserRouter>
       <div style={{ marginTop: '35px', textAlign: 'center' }}>
-        
         <button onClick={togglePopup} style={{
           backgroundColor: '#081d48', color: 'white', padding: '10px 20px', border: 'none', borderRadius: '20px', cursor: 'pointer', fontSize: '16px'
-        }}>About me</button>
+        }}>About Me</button>
         {showPopup && <AboutMePopup />}
       </div>
-      <p className='text-emboss'>If you like Music, please share it ! <ShareButton /></p>
+      <p className="text-emboss">If you like Music, please share it! <ShareButton /></p>
       <ToastContainer />
     </>
   );
