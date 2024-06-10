4ef6304ae6133cfb467133ee9e61b48108526a7f
Yassine Hamza <yassine.hamza@esprit.tn>
2024-06-10 20:04:01 +0100

    commit129

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 // CORS options to allow specific origins
 const corsOptions = {
-    origin: ['https://melodyaddicts.netlify.app', 'https://musicdownloader1.onrender.com'],
+    origin: ['https://melodyaddicts.netlify.app', 'https://songsdownloader.onrender.com'],
     optionsSuccessStatus: 200 // For legacy browser support
 };
 
