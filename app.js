4073a2bce86a782b7ffb0d9bead57ea206028b68
Yassine Hamza <yassine.hamza@esprit.tn>
2024-06-12 01:18:42 +0100

    commit154

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('trust proxy', 1);
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     message: 'Too many requests from this IP, please try again after 15 minutes'
   });
 app.use(limiter);
--- routes/music.js
+++ routes/music.js
@@ -6,7 +6,7 @@ const rateLimit = require('express-rate-limit');
 //rate limiter 
 const musicRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10, // Limit each IP to 10 requests per windowMs
+  max: 5, // Limit each IP to 10 requests per windowMs
   message: 'Too many requests from this IP, please try again after 15 minutes'
 });
 
